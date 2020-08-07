f473b3af04ec497734995cfd83e85db17204b713
AJavier55 <aliciajavier5@gmail.com>
2020-08-07 11:57:33 -0400

    bank_account has-many transfers

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -1,3 +1,30 @@
-class BankAccount
+    class BankAccount
 
-end
+        attr_accessor :status, :balance
+        attr_reader :name
+    
+    
+        def initialize(name, balance = 1000, status = "open")
+            @name = name
+            @balance = balance
+            @status = status
+        end
+    
+        def deposit(deposit)
+            self.balance += deposit
+        end
+    
+        def display_balance
+            "Your balance is $#{self.balance}."
+        end
+    
+        def valid?
+            self.status == "open" && self.balance > 0
+        end
+    
+        def close_account
+            self.status = "closed"
+        end
+    
+    end
+
