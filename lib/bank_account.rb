3b90c3d83118a078d8f0d82db5078cfde1a520f2
D1yr <alexander.m.sahli@gmail.com>
2020-08-07 11:04:21 -0400

    halfway done transfer

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -1,10 +1,12 @@
 class BankAccount
     attr_reader :name
     attr_accessor :balance, :status
+    @@all = []
     def initialize(name)
         @name = name
         @balance = 1000
         @status = 'open'
+        @@all<< self
     end
     def deposit(num)
         @balance += num
@@ -18,5 +20,7 @@ class BankAccount
     def close_account
         @status = 'closed'
     end
-
+    def self.all
+        @@all
+    end
 end
--- lib/transfer.rb
+++ lib/transfer.rb
@@ -1,3 +1,20 @@
 class Transfer
-  # your code here
+  attr_accessor :sender, :receiver, :amount, :status
+  def initialize(sender, receiver, amount)
+    @sender = sender
+    @receiver = receiver
+    @status = 'pending'
+    @amount = amount
+  end
+  def valid?
+    BankAccount.all.select do |name|
+      if name.valid? == @sender ||= @receiver
+        return true
+    end
+  end
+  return false
+  end
+  def execute_transaction
+
+  end
 end
