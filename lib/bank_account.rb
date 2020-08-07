23f21aad271451116f027235b22a38ff7cd82781
anickel101 <anson.nickel@gmail.com>
2020-08-07 11:10:17 -0400

    Completed Lab

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -1,3 +1,40 @@
 class BankAccount
 
+    attr_reader :name
+    attr_accessor :balance, :status
+
+    @@all = []
+
+    def self.all
+        @@all
+    end
+
+    def initialize(name)
+        @name = name
+        @balance = 1000
+        @status = "open"
+        @@all << self
+
+    end
+
+    def deposit(amount)
+        self.balance += amount
+        
+    end
+
+    def withdraw(amount)
+        self.balance -= amount
+    end
+
+    def display_balance
+        "Your balance is $#{self.balance}."
+    end
+
+    def valid?
+        self.status == "open" && self.balance > 0
+    end
+
+    def close_account
+        self.status = "closed"
+    end
 end
